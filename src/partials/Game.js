fad7b139f05b3bea152faf8e96d62e4b878270ff
corawym <corawym@gmail.com>
2017-08-03 16:41:02 -0700

    created board, paddles and ball

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,66 @@
+import {SVG_NS, KEYS} from '../settings';
+import Board from './Board';
+import Paddle from './Paddle';
+import Ball from './Ball';
+
 export default class Game {
 
 	constructor(element, width, height) {
-		this.element = element;
+
 		this.width = width;
 		this.height = height;
 	
-		// Other code goes here...
+		this.gameElement = document.getElementById(element);
+
+		this.paddleWidth = 8;
+		this.paddleHeight = 52;
+		this.boardGap = 10,
+
+		this.board = new Board(this.width, this.height);
+		this.player1 = new Paddle(
+			this.height, 
+			this.paddleWidth, 
+			this.paddleHeight,
+			this.boardGap,
+			(this.height - this.paddleHeight)/2,
+			KEYS.a,
+			KEYS.z
+		);
+		this.player2 = new Paddle(
+			this.height, 
+			this.paddleWidth, 
+			this.paddleHeight,
+			this.width - this.paddleWidth - this.boardGap,
+			(this.height - this.paddleHeight)/2,
+			KEYS.up,
+			KEYS.down
+		);
+
+		this.ballRadius = 8;
+		// <circle class="ball" cx="251" cy="128" r="8" fill="white"/>
+
+		this.ball = new Ball(
+			this.ballRadius,
+			this.width,
+			this.height
+		);
+
 	}
 
 	render() {
-		// More code goes here...
+		this.gameElement.innerHTML = '';
+
+		let svg = document.createElementNS(SVG_NS,'svg');
+		svg.setAttributeNS(null, 'version', '1.1');
+		svg.setAttributeNS(null, 'width', this.width);
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+		this.gameElement.appendChild(svg);
+
+		this.board.render(svg);
+		this.player1.render(svg);
+		this.player2.render(svg);
+		this.ball.render(svg);
 	}
 
 }
---
+++ src/partials/Paddle.js
@@ -0,0 +1,43 @@
+import {SVG_NS} from '../settings';
+
+export default class Paddle {
+  constructor(boardHeight, width, height, x, y, up, down) {
+    this.boardHeight = boardHeight;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.speed = 10;
+    this.score = 0;
+
+    document.addEventListener('keydown', event => {
+      switch(event.key) {
+        case up:
+          this.up();
+          break;
+        case down:
+          this.down();
+          break;
+      }
+    });
+  }
+  up(){
+    // get the max number
+    this.y = Math.max(this.y - this.speed, 0);
+
+  }
+  down(){
+    // get the min number
+    this.y = Math.min(this.y + this.speed, this.boardHeight - this.height);
+  }
+
+  render(svg){
+    let rect = document.createElementNS(SVG_NS,'rect');
+    rect.setAttributeNS(null, 'x', this.x); //
+    rect.setAttributeNS(null, 'y', this.y); //
+    rect.setAttributeNS(null, 'width', this.width);
+    rect.setAttributeNS(null, 'height', this.height);
+    rect.setAttributeNS(null, 'fill', 'white');
+    svg.appendChild(rect);
+  }
+} // end class Paddle
